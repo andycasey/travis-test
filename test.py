3358e7a1b0c01032e5bf5059452f9cbf8f59e260
Andy Casey <andycasey@gmail.com>
2014-12-23 20:33:26 +1100

    2to3 fix

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+from future import print_function
+
 import numpy
 import scipy
+import matplotlib.pyplot as plt
 
-print numpy.__version__
-print scipy.__version__
+print("numpy version {}".format(numpy.__version__))
+print("scipy version {}".format(scipy.__version__))
+
+
+fig, ax = plt.subplots()
+ax.plot([1,5], [1,5], "k:")
+fig.savefig("what.png")
