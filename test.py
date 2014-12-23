01f4a696be35293826952993b81578351ae7f499
Andy Casey <andycasey@gmail.com>
2014-12-23 20:35:22 +1100

    typo

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from future import print_function
+# coding: utf-8
+
+from __future__ import print_function
 
 import numpy
 import scipy
@@ -6,7 +8,6 @@ import matplotlib.pyplot as plt
 
 print("numpy version {}".format(numpy.__version__))
 print("scipy version {}".format(scipy.__version__))
-
 
 fig, ax = plt.subplots()
 ax.plot([1,5], [1,5], "k:")
